958cca13c348474e7414704d15bcfbc24985c43e
jwasacz <kuba.wasacz160601@wp.pl>
2025-04-13 14:03:27 +0200

    TMDB API

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,59 +4,54 @@ import { Tabs } from "expo-router";
 import { images } from "@/constants/images";
 import { icons } from "@/constants/icons";
 
-const TabIcon = ({focused, icon, title}: any) => {
-  if(focused){
+const TabIcon = ({ focused, icon, title }: any) => {
+  if (focused) {
+    return (
+      <>
+        <ImageBackground
+          source={images.highlight}
+          className="flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden"
+        >
+          <Image source={icon} tintColor="#151312" className="size-5"></Image>
+          <Text className="text-secondary text-base font-semibold ml-2">
+            {title}
+          </Text>
+        </ImageBackground>
+      </>
+    );
+  }
+
   return (
-    <>
-      <ImageBackground
-        source={images.highlight}
-        className="flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden"
-      >
-        <Image
-          source={icon}
-          tintColor="#151312"
-          className="size-5"
-        ></Image>
-        <Text className="text-secondary text-base font-semibold ml-2">
-          {title}
-        </Text>
-      </ImageBackground>
-    </>
+    <View className="size-full justify-center items-center mt-4 rounded-full">
+      <Image source={icon} tintColor="#A8B5DB" className="size-5" />
+    </View>
   );
 };
-
-return(
-  <View className="size-full justify-center items-center mt-4 rounded-full">
-    <Image source={icon} tintColor="#A8B5DB" className="size-5"/>
-  </View>
-)
-}
 
 const _layout = () => {
   return (
     <Tabs
-    screenOptions={{
-      tabBarShowLabel: false,
-      tabBarItemStyle: {
-        width: '100%',
-        height: '100%',
-        justifyContent: 'center',
-        alignItems: "center",
-      },
-      tabBarStyle:{
-        backgroundColor: '#0f0D23',
-        borderRadius: 50,
-        marginHorizontal: 20,
-        marginBottom: 36,
-        height: 52,
-        position: 'absolute',
-        overflow: 'hidden',
-        borderWidth: 1,
-        borderColor: '0F0D23'
-      }
-    }}
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarItemStyle: {
+          width: "100%",
+          height: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        },
+        tabBarStyle: {
+          backgroundColor: "#0f0D23",
+          borderRadius: 50,
+          marginHorizontal: 20,
+          marginBottom: 36,
+          height: 52,
+          position: "absolute",
+          overflow: "hidden",
+          borderWidth: 1,
+          borderColor: "0F0D23",
+        },
+      }}
     >
-
       <Tabs.Screen
         name="index"
         options={{
@@ -64,6 +59,17 @@ const _layout = () => {
           headerShown: false,
           tabBarIcon: ({ focused }) => (
             <TabIcon focused={focused} icon={icons.home} title="Home" />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: "Wyszukaj",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.search} title="Wyszukaj" />
           ),
         }}
       />
@@ -86,17 +92,6 @@ const _layout = () => {
           headerShown: false,
           tabBarIcon: ({ focused }) => (
             <TabIcon focused={focused} icon={icons.save} title="Zapisane" />
-          ),
-        }}
-      />
-
-      <Tabs.Screen
-        name="search"
-        options={{
-          title: "Wyszukaj",
-          headerShown: false,
-          tabBarIcon: ({ focused }) => (
-            <TabIcon focused={focused} icon={icons.search} title="Wyszukaj" />
           ),
         }}
       />
